779d917eefb4d9afa3641fb2e00552440f03e334
Taylor Hans <taylor.hans7@gmail.com>
2020-02-17 22:20:50 -0500

    Initial Commit - MVCSetup

---
+++ Hans.WebTools/Helpers/ControllerHelpers.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Hans.WebTools.Helpers
+{
+    /// <summary>
+    ///  Any helpers that are useful for controller management.  Assists in async management, and more.
+    /// </summary>
+    public static class ControllerHelpers
+    {
+        /// <summary>
+        ///  Asyncronously calls the method's view so the controller can handle it properly.
+        /// </summary>
+        /// <param name="thisController">The controller that's being called upon.</param>
+        /// <returns>The view pertaining to this method being called.</returns>
+        public static async Task<ActionResult> ViewAsync(this Controller thisController)
+        {
+            return await Task.Run(() => thisController.View());
+        }
+    }
+}
---
+++ PatternCreator/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Hans.WebTools.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace PatternCreator.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        ///  Default Home Page.
+        /// </summary>
+        public async Task<ActionResult> Index()
+        {
+            return await this.ViewAsync();
+        }
+    }
+}
