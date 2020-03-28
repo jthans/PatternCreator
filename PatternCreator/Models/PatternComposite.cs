ed4866a580e61a036462608e233c496349fcb36b
Taylor Hans <taylor.hans7@gmail.com>
2020-03-27 23:28:34 -0400

    Pattern Composite Creation

---
+++ PatternCreator/Models/PatternComposite.cs
@@ -0,0 +1,22 @@
+﻿using Hans.Math.Geometry.Models;
+using System.Collections.Generic;
+
+namespace PatternCreator.Models
+{
+    /// <summary>
+    ///  Composite model describing the polygons that lie within, and any description associated with it.
+    /// </summary>
+    public class PatternComposite
+    {
+        /// <summary>
+        ///  Name of the pattern that's represented by this model.
+        /// </summary>
+        public string PatternName { get; set; }
+
+        /// <summary>
+        ///  Collection of polygons in this pattern, along with the number of iterations
+        ///     that the polygon occurs in the pattern.
+        /// </summary>
+        public Dictionary<Polygon, int> Polygons { get; set; }
+    }
+}
